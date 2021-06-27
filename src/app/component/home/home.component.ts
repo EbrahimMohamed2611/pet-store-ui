4120085c96b183d4123f96d32b0da7b04c05ff0e
Mahmoud Ali Khalil <mahmoud.alikhalil@gmail.com>
2021-06-27 09:23:40 +0200

    Fixed home page.

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,15 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {CategoryService} from "../../service/category/category.service";
-import {Category} from "../../model/Category.model";
-import {ToastrService} from "ngx-toastr";
-import {HttpErrorResponse} from "@angular/common/http";
-import {ProductService} from "../../service/product/product.service";
-import {Product} from "../../model/Product.model";
-import {Products} from "../../model/Products.model";
-import {CartService} from "../../service/cart/cart.service";
-import {CartItem} from "../../model/CartItem.model";
-import { AuthenticationService } from 'src/app/service/authenticate/authentication.service';
-import { Router } from '@angular/router';
+import {Category} from '../../model/Category.model';
+import {ToastrService} from 'ngx-toastr';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Product} from '../../model/Product.model';
+import {CartService} from '../../service/cart/cart.service';
+import {CartItem} from '../../model/CartItem.model';
+import {AuthenticationService} from 'src/app/service/authenticate/authentication.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -17,62 +14,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  private categorySize: number = 3;
-  private rateSize: number = 3;
   public categories: Category[] = [];
   public categoryProducts: Product[] = [];
   public topRateProducts: Product[] = [];
   public bestOfferProducts: Product[] = [];
 
-  constructor(private categoryService: CategoryService,
-              private productService: ProductService,
-              private shoppingCartService: CartService,
+  constructor(private shoppingCartService: CartService,
               private notification: ToastrService,
-              private _authService:AuthenticationService,
-              private _routerService:Router) {
+              private _authService: AuthenticationService,
+              private route: ActivatedRoute,
+              private _routerService: Router) {
   }
 
   ngOnInit(): void {
-    this.getTheTopThreeCategory();
-    this.getAllProductsInHome();
-    this.getTopRatedProducts();
-    this.getTopOfferProducts();
-  }
-
-
-  private getTheTopThreeCategory(): void {
-    this.categoryService.getTheFirstThreeCategories(this.categorySize).subscribe((categories: Category[]) => {
-      this.categories = categories;
-    }, (error: HttpErrorResponse) => {
-      this.notification.error(error.error.message)
-    })
-
-  }
-
-  public getCategoryProducts(categoryId: number): void {
-    this.productService.getCategoryProducts(categoryId).subscribe((products: Products) => {
-      this.categoryProducts = products.products;
-    }, (error: HttpErrorResponse) => {
-      this.notification.error(error.error.message)
-    })
-  }
-
-  public getAllProductsInHome(): void {
-    this.productService.getProducts().subscribe((products: Products) => {
-      this.categoryProducts = products.products;
-    }, (error: HttpErrorResponse) => {
-      this.notification.error(error.error.message)
-    })
+    this.topRateProducts = this.route.snapshot.data.topRates;
+    this.categoryProducts = this.route.snapshot.data.products;
+    this.bestOfferProducts = this.route.snapshot.data.topOffers;
+    this.categories = this.route.snapshot.data.topCategories;
   }
 
   public addToShoppingCart(product: Product): void {
-    if (!this._authService.isLoggedIn()){
-      this._routerService.navigateByUrl("/login");
-    }else{
+    if (!this._authService.isLoggedIn()) {
+      this._routerService.navigateByUrl('/login');
+    } else {
 
       this.shoppingCartService.updateShoppingCart(product, 1)
         .subscribe((cartItems: CartItem[]) => {
-          console.log("cartItems " ,cartItems)
+          console.log('cartItems', cartItems);
           cartItems.forEach(items => {
             if (items.product.id == product.id) {
               this.notification.info('your cart has ' + items.quantity + ' from ' + items.product.name);
@@ -80,24 +48,8 @@ export class HomeComponent implements OnInit {
           });
         }, (error: HttpErrorResponse) => {
           // console.error("error " ,error)
-          this.notification.error(error.error.message)
+          this.notification.error(error.error.message);
         });
     }
-  }
-
-  private getTopRatedProducts(): void {
-    this.productService.getTopRatedProducts(this.rateSize).subscribe((products: any) => {
-      this.topRateProducts = products.products;
-    }, (error: HttpErrorResponse) => {
-      this.notification.error(error.error.message)
-    })
-  }
-
-  private getTopOfferProducts(): void {
-    this.productService.getTopRatedProducts(this.rateSize).subscribe((products: any) => {
-      this.bestOfferProducts = products.products;
-    }, (error: HttpErrorResponse) => {
-      this.notification.error(error.error.message)
-    })
   }
 }
---
+++ src/app/resolvers/home-products-resolver.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {ProductService} from '../service/product/product.service';
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
+import {EMPTY, Observable} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeProductsResolverService implements Resolve<any> {
+
+  constructor(private productService: ProductService, private router: Router) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+    setTimeout(() => { console.log('World!'); }, 60000);
+    return this.productService.getProducts()
+      .pipe(
+        map(response => response.products),
+        catchError(e => {
+        this.router.navigateByUrl('/notfound');
+        return EMPTY;
+      }));
+  }
+}
